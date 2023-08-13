dc7455412f50e7a227fbf5a49c114452310dbafb
king-odysseus <wingzatari@gmail.com>
2023-08-13 20:47:07 +0100

    cards tooltip

--- javascript.js
+++ javascript.js
@@ -1,10 +1,10 @@
-const card1 = document.querySelector(".card1");
-const customTooltip = document.getElementById("custom-tooltip");
+// const card1 = document.querySelector(".card1");
+// const customTooltip = document.getElementById("custom-tooltip");
 
-card1.addEventListener("mouseenter", () => {
-  customTooltip.style.display = "block";
-});
+// card1.addEventListener("mouseenter", () => {
+//   customTooltip.style.display = "block";
+// });
 
-card1.addEventListener("mouseleave", () => {
-  customTooltip.style.display = "none";
-});
+// card1.addEventListener("mouseleave", () => {
+//   customTooltip.style.display = "none";
+// });
