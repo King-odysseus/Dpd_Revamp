4e7e8bf945adb707d1911852ea3125aa5a3e147c
Nate <96822493+King-odysseus@users.noreply.github.com>
2023-08-16 13:39:01 +0000

    footer

--- javascript.js
+++ javascript.js
@@ -8,3 +8,18 @@
 // card1.addEventListener("mouseleave", () => {
 //   customTooltip.style.display = "none";
 // });
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
